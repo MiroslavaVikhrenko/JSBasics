1dfa626e9ad938043ff1097406ea983c8a4353e9
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-06-30 09:26:58 -0600

    Practice arrays basics

--- 20250628.js
+++ 20250628.js
@@ -17,3 +17,14 @@ const ProductsOutOfStock = products.filter((product, index) =>  {
 
 console.log(instockProducts)
 console.log(ProductsOutOfStock)
+
+// TASK 2 Write a function that sums all the salaries:
+const users = [
+  { id: 100, name: "Anna", salary: 2345 },
+  { id: 101, name: "Bob", salary: 3421 },
+  { id: 102, name: "Ava", salary: 1234 },
+]; 
+
+const sum = users.reduce(function () {
+    
+})
